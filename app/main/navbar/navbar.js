27bb68542bba144e23bc828b59fa668789b8d7a8
Vasanth Senthil <vstzen@gmail.com>
2022-11-15 20:23:15 -0800

    Created Navbar HTML and CSS, modified landing.html to display it

---
+++ app/main/navbar/navbar.js
@@ -0,0 +1,18 @@
+class NavBar extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        <head>
+          <link rel="stylesheet" href="../navbar/navbar.css">
+        </head>
+        <div>
+          <img id = "Logo" src="../../../assets/navbarLogo.png" alt="Logo">
+        </div>
+      `;
+    }
+  }
+  
+  customElements.define('navbar-component', NavBar);
