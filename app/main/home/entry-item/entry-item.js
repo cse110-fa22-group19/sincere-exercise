7a0a048be590bafa4bd76af53e01c3af36941e56
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2022-11-18 09:14:10 -0800

    Removed .idea and reformatted spaces

--- app/main/home/entry-item/entry-item.js
+++ app/main/home/entry-item/entry-item.js
@@ -8,10 +8,10 @@ class EntryItem extends HTMLElement {
       <head>
         <link rel="stylesheet" href="/app/main/home/entry-item/entry-item.css">
       </head>
-        <section class="entry-component">
-          <label class="entry-label">Entry</label>
-          <label class="date-label">September 30, 2022</label>
-        </section>
+      <section class="entry-component">
+        <label class="entry-label">Entry</label>
+        <label class="date-label">September 30, 2022</label>
+      </section>
     `;
   }
 }
