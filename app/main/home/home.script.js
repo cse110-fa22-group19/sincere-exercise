dbb95616c6322814005d04c9123068bc915af3f7
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2022-11-22 10:48:45 -0800

    Delete entries from DOM

--- app/main/home/home.script.js
+++ app/main/home/home.script.js
@@ -3,30 +3,47 @@ import { entryEntity } from '../../core/entry.entity.js';
 window.addEventListener('DOMContentLoaded', () => {
   // getAllEntries here
   console.log('loaded');
-  addEntryToList();
-  // createEntryItem();
-  // entryUpdates();
+  addEntryEvent();
+  getAllEntriesEvent();
 });
 
-function addEntryToList() {
-  const newEntryButton = document.querySelector('new-entry-button');
-  newEntryButton.addEventListener('addNewEntry', () => {
-    const entryItem = createEntryItem();
-    const entryList = document.querySelector('#entry-list');
-    entryList.appendChild(entryItem);
+/**
+ * Gets all entries from localStorage and displays them on the home page.
+ */
+function getAllEntriesEvent() {
+  const existingEntries = entryEntity.getAllEntries();
+  existingEntries.forEach((entry) => {
+    createEntryItem(entry);
   });
 }
 
-function createEntryItem() {
-  const newEntryEntity = entryEntity.createEntry();
+/**
+ * Creates a new entry entity and displays the saved entry on the home page.
+ */
+function addEntryEvent() {
+  const newEntryButton = document.querySelector('new-entry-button');
+  newEntryButton.addEventListener('addNewEntry', () => {
+    const newEntryEntity = entryEntity.createEntry();
+    const newEntryItem = createEntryItem(newEntryEntity);
+    appendEntryToList(newEntryItem);
+  });
+}
+
+/**
+ * Creates an entry-item-component and appends the component to the entry-list
+ * @param {Entry} entry - the entry entity object to set to data of new entry item
+ */
+function createEntryItem(entry) {
   const entryItem = document.createElement('entry-item-component');
-  entryItem.setAttribute('data', newEntryEntity);
+  const entryList = document.querySelector('#entry-list');
+  entryItem.setAttribute('data', entry);
   entryItem.addEventListener('deleteEntry', () => {
-    if (!!newEntryEntity) {
-      entryEntity.deleteEntry(newEntryEntity.__id);
+    if (!!entry) {
+      entryEntity.deleteEntry(entry.__id);
+      entryItem.remove();
     } else {
       console.error(`ERROR: entity data object of this entry does not exist!`);
     }
   });
-  return entryItem;
+  entryList.appendChild(entryItem);
 }
