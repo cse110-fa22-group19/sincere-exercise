8206cc5a92513f2df660bb421d01f312b11940b3
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2022-11-23 22:14:21 -0800

    Completed home E2E tests

--- __tests__/home.test.js
+++ __tests__/home.test.js
@@ -15,17 +15,17 @@ describe('Home end to end user flows', () => {
 
   describe('Create and delete entries', () => {
     let home;
+    let newEntryButtonElement;
     let newEntryButton;
-    let button;
 
     beforeAll(async () => {
       home = await page.$('home-page');
-      newEntryButton = await home.$('new-entry-button');
-      button = await newEntryButton.$('button');
+      newEntryButtonElement = await home.$('new-entry-button');
+      newEntryButton = await newEntryButtonElement.$('button');
     });
 
     test('when click Add New Entry button, new entry item is shown', async () => {
-      await button.click();
+      await newEntryButton.click();
       const numEntries = await home.$$eval(
         'entry-item-component',
         (entryItems) => entryItems.length
@@ -44,9 +44,9 @@ describe('Home end to end user flows', () => {
       expect(entryItemData).toEqual(entity);
     });
 
-    test('when more entries are added, number of entries and entry data are accurate', async () => {
+    test('when more entries are added, entry count and data are accurate', async () => {
       for (let i = 0; i < 9; i++) {
-        await button.click();
+        await newEntryButton.click();
       }
       const numEntries = await home.$$eval(
         'entry-item-component',
@@ -60,6 +60,39 @@ describe('Home end to end user flows', () => {
         Object.values(JSON.parse(localStorage.getItem('entries')))
       );
       expect(numEntries).toBe(10);
+      expect(entryItemData).toEqual(entities);
+    });
+
+    test('when entry is deleted, correct entry is deleted', async () => {
+      const entryItemToDelete = await home.$('entry-item-component');
+      const deleteButton = await entryItemToDelete.$('button');
+      await deleteButton.evaluate((b) => b.click());
+
+      const entryItemData = await home.$$eval(
+        'entry-item-component',
+        (entryItems) => entryItems.map((entryItem) => entryItem.entryData)
+      );
+      const entities = await page.evaluate(() =>
+        Object.values(JSON.parse(localStorage.getItem('entries')))
+      );
+      expect(entryItemData.length).toBe(9);
+      expect(entryItemData).toEqual(entities);
+    });
+
+    test('when all entries are deleted, no entries in localStorage', async () => {
+      const entryItems = await home.$$('entry-item-component');
+      for (let entryItem of entryItems) {
+        const deleteButton = await entryItem.$('button');
+        await deleteButton.evaluate((b) => b.click());
+      }
+      const entryItemData = await home.$$eval(
+        'entry-item-component',
+        (entryItems) => entryItems.map((entryItem) => entryItem.entryData)
+      );
+      const entities = await page.evaluate(() =>
+        Object.values(JSON.parse(localStorage.getItem('entries')))
+      );
+      expect(entryItemData.length).toBe(0);
       expect(entryItemData).toEqual(entities);
     });
   });
